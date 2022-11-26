2aedc8c45577c2bae36860f697d5e9aca3134a26
Alina-KI <kraynova.alina.2002@mail.ru>
2022-11-26 09:53:23 +0300

    added header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
- <Header/>
+  <Header />
 </template>
 
 <script lang="ts">
@@ -8,7 +8,7 @@ import Header from '@/components/Header.vue'
 
 @Options({
   components: {
-    Header,
+    Header
   }
 })
 export default class App extends Vue {
@@ -16,12 +16,19 @@ export default class App extends Vue {
 </script>
 
 <style lang="scss">
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  max-width: 1390px;
+  width: 100%;
+  height: 100vh;
+  margin: 0 auto;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="header">
+    <img src="@/assets/images/Logo.svg" alt="my-logo" />
+    <div class="blockImages">
+      <img src="@/assets/images/Person.svg" alt="">
+      <img src="@/assets/images/Favorite.svg" alt="">
+      <img src="@/assets/images/Bag.svg" alt="">
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+
+export default {
+  name: 'Header'
+
+}
+</script>
+
+<style scoped>
+.header {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  height: 40px;
+  width: 100%;
+}
+
+.blockImages {
+  flex-grow: 1;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 25px;
+}
+</style>
