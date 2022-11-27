05201772ee41cb7d4007e72c462c797f84f829c2
Alina-KI <kraynova.alina.2002@mail.ru>
2022-11-28 00:11:39 +0300

    added Product

---
+++ src/components/Product.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="product">
+    <div class="container">
+      <div class="block">
+        <ImageBlock />
+        <div class="info">
+          <ProductInfo />
+          <div class="line" />
+          <AdditionalInfo />
+        </div>
+      </div>
+      <a class="link" href="#">Посмотреть все стили</a>
+      <Gallery />
+    </div>
+  </div>
+</template>
+
+<script>
+import Gallery from '@/components/Gallery'
+import ProductInfo from '@/components/ProductInfo'
+import AdditionalInfo from '@/components/AdditionalInfo'
+import ImageBlock from '@/components/ImageBlock'
+
+export default {
+  name: 'Product',
+  components: { ImageBlock, AdditionalInfo, ProductInfo, Gallery }
+}
+</script>
+
+<style scoped>
+.product {
+  max-width: 1390px;
+  width: 100%;
+  margin: 0 auto;
+}
+
+.container{
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  margin-bottom: 130px;
+  margin-top: 25px;
+}
+
+.block {
+  display: flex;
+  flex-direction: row;
+  gap: 15px;
+  flex-wrap: wrap;
+}
+
+.info {
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+}
+
+.line {
+  width: 100%;
+  height: .5px;
+  background: #C4C4C4;
+}
+
+.link{
+  font-family: 'Open Sans', sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 20px;
+  letter-spacing: 0.04em;
+  text-decoration-line: underline;
+  color: #333333;
+}
+
+@media (max-width: 1105px) {
+  .block {
+    justify-content: center;
+  }
+  .info{
+    width: 100%;
+  }
+}
+@media (min-width: 1105px) {
+  .line{
+    max-width: 686px;
+  }
+}
+@media (min-width: 1105px) {
+  .info{
+    min-width: 500px;
+  }
+}
+</style>
---
+++ src/components/ProductInfo.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="infoProduct">
+    <InfoBlock />
+    <PriceBlock />
+    <CartOrFavoriteBlock />
+  </div>
+</template>
+
+<script>
+import PriceBlock from '@/components/PriceBlock'
+import InfoBlock from '@/components/InfoBlock'
+import CartOrFavoriteBlock from '@/components/CartOrFavoriteBlock'
+import AdditionalInfo from '@/components/AdditionalInfo'
+
+export default {
+  name: 'ProductInfo',
+  components: { CartOrFavoriteBlock, InfoBlock, PriceBlock }
+}
+</script>
+
+<style scoped>
+.infoProduct {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+  align-items: flex-start;
+  font-family: 'Open Sans', sans-serif;
+  font-style: normal;
+  padding: 0 20px;
+}
+@media (max-width: 1105px) {
+  .infoProduct {
+    max-width: 500px;
+    width: 100%;
+    margin: 0 auto;
+  }
+}
+</style>
